5337381176de0d0b0b3897b2c259759801a8ac87
kermekx <kermekx@gmail.com>
2016-01-03 21:09:31 +0100

    Anti-Aliasing
    MSAA-8 PC
    MSAA-4 Android

--- desktop/src/com/kermekx/zombiesurvival/desktop/DesktopLauncher.java
+++ desktop/src/com/kermekx/zombiesurvival/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.kermekx.zombiesurvival.ZombieSurvival;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.samples = 8;
 		new LwjglApplication(new ZombieSurvival(), config);
 	}
 }
